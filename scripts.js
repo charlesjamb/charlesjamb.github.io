89e63a055d65421f28ab02acf4a0d5c867706b75
Charles Jambon <charles.jambon@gmail.com>
2021-04-27 22:55:55 +0200

    Add support for dark theme

--- scripts.js
+++ scripts.js
@@ -90,26 +90,36 @@ for (let i = 0; i < usedSkills.length; i++) {
 }
 
 // Toggle dark mode
-// const btn = document.querySelector('button#dark-mode')
-// const currentTheme = localStorage.getItem('theme')
+const btn = document.querySelector('button#dark-mode')
+const currentTheme = localStorage.getItem('theme')
+const sunIcon =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"/></svg>'
+const moonIcon =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-6.671-5.575A8 8 0 1 0 16.425 5.328a8.997 8.997 0 0 1-2.304 8.793 8.997 8.997 0 0 1-8.792 2.304z"/></svg>'
 
-// if (
-//   currentTheme === 'dark' ||
-//   (!currentTheme && window.matchMedia('(prefers-color-scheme: dark)').matches) // check user browser setting for dark mode
-// ) {
-//   document.body.classList.add('dark-mode')
-// }
-// // Listen for a click on the button
-// btn.addEventListener('click', function () {
-//   document.body.classList.toggle('dark-mode')
+btn.innerHTML = ''
+if (
+  currentTheme === 'dark' ||
+  (!currentTheme && window.matchMedia('(prefers-color-scheme: dark)').matches) // check user browser setting for dark mode
+) {
+  document.body.classList.add('dark-mode')
+  btn.insertAdjacentHTML('afterbegin', sunIcon)
+} else {
+  btn.insertAdjacentHTML('afterbegin', moonIcon)
+}
 
-//   // Let's say the theme is equal to light
-//   let theme = 'light'
-//   // If the body contains the .dark-theme class...
-//   if (document.body.classList.contains('dark-mode')) {
-//     // ...then let's make the theme dark
-//     theme = 'dark'
-//   }
-//   // Then save the choice in localStorage
-//   localStorage.setItem('theme', theme)
-// })
+btn.addEventListener('click', function () {
+  btn.innerHTML = ''
+  document.body.classList.toggle('dark-mode')
+
+  let theme = 'light'
+  let icon = moonIcon
+
+  if (document.body.classList.contains('dark-mode')) {
+    theme = 'dark'
+    icon = sunIcon
+  }
+
+  localStorage.setItem('theme', theme)
+  btn.insertAdjacentHTML('afterbegin', icon)
+})
